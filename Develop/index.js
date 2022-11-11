5bef1d79b59763e3ed339f68c480b2cb932e49a2
karasu yami <demigodlinty@gmail.com>
2022-11-10 23:19:31 -0600

    added repo

---
+++ Develop/index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+   const inquierer = require('inquirer')
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    name: 'readName',
+    type: 'input',
+    message:'what is title of your app'
+},
+
+   {
+    name: 'description',
+    type: 'type',
+    message: 'provide a short description of your app'
+   }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function fo initialize app
+function init() {}
+
+// Function call 
